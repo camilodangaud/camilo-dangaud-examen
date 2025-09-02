497d51fa00a47634b26892f2dd9062da0a5a5f07
camilodangaud <143123036+camilodangaud@users.noreply.github.com>
2025-09-02 14:24:59 -0500

    proyecto final

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,8 +6,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss'],
   standalone: false,
 })
-export class CardComponent  {
+export class CardComponent {
   @Input() title: string = '';
   @Input() subtitle: string = '';
   @Input() content: string = '';
+  @Input() url: string = '';
+
+  openNews() {
+    if (this.url) {
+      window.open(this.url, '_blank');
+    }
+  }
 }
